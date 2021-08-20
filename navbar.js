945e5d182c96ed23edd48a0be6a864b58839ba1d
Aidan <aidanschang@gmail.com>
2021-08-20 00:07:02 -0700

    signup link

--- navbar.js
+++ navbar.js
@@ -10,7 +10,7 @@ class pageHeader extends HTMLElement {
                 <li><a href="#team_member_container" class="menubar__links" id="team_page">team</a></li>
                 <li><a href="#courses_container" class="menubar__links" id="courses_page">courses</a></li>
                 <li><a href="#projects" class="menubar__links" id="projects_page">projects</a></li>
-                <li><a href="#signup_container" class="menubar__links" id ="signup_button" target="_blank">Sign Up</a></li>
+                <li><a href="signup.html" class="menubar__links" id ="signup_button" target="_blank">Sign Up</a></li>
             </ul>
         </nav>
         <label for='nav-toggle' class='nav-toggle-label' id="mobile-menu">
