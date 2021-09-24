c9699a43fa1a202bdeb9a576e6a29bfc7edc7024
Aidan <aidanschang@gmail.com>
2021-09-23 22:10:08 -0700

    video_loader_by_type

--- project_video_loader.js
+++ project_video_loader.js
@@ -1,6 +1,7 @@
 
-const videoContent = document.getElementById('project_row');
-
+const scratchContent = document.getElementById('scratch_row');
+const pythonContent = document.getElementById('python_row');
+const robloxContent = document.getElementById('roblox_row');
 //THE REQUEST
 const xhr = new XMLHttpRequest();
 xhr.open('GET', 'http://39.108.187.78:5814/home/videoList?pageIndex=1', true);
@@ -12,29 +13,67 @@ xhr.onreadystatechange = function() {
         console.log(xhr.responseText)
         const myData = JSON.parse(xhr.responseText).data.Videos;
 
-        let videos = "";
+        let scratchVideos = "";
+        let pythonVideos = "";
+        let robloxVideos = "";
+
         // looping each videos
         myData.forEach( video => {
-            videos += `
-            <div class="individual_project">
+            if(video.type ==="scratch") {
+                scratchVideos += `
+                <div class="individual_project">
+                    <video controls class=project_videos>
+                        <div class="individual_project"></div>
+                        <source src="http://39.108.187.78:5814${video.url}" type="video/mp4">
+                    </video>
+                    <p class="project_title">
+                        ${video.title}
+                    </p>
+                    <p class="project_description">
+                        video.video_description
+                    </p>
+                </div>`
+            }
+
+            if(video.type ==="python") {
+                pythonVideos += `
+                <div class="individual_project">
+                    <video controls class=project_videos>
+                        <div class="individual_project"></div>
+                        <source src="http://39.108.187.78:5814${video.url}" type="video/mp4">
+                    </video>
+                    <p class="project_title">
+                        ${video.title}
+                    </p>
+                    <p class="project_description">
+                        video.video_description
+                    </p>
+                </div>`
+            }
+            
+            if(video.type ==="roblox") {
+                robloxVideos += `
                 <video controls class=project_videos>
-                    <div class="individual_project"></div>
-                    <source src="http://39.108.187.78:5814${video.url}" type="video/mp4">
-                </video>
-                <p class="project_title">
-                    ${video.title}
-                </p>
-                <p class="project_description">
-                    video.video_description
-                </p>
-            </div>`
+                        <div class="individual_project"></div>
+                        <source src="http://39.108.187.78:5814${video.url}" type="video/mp4">
+                    </video>
+                    <p class="project_title">
+                        ${video.title}
+                    </p>
+                    <p class="project_description">
+                        video.video_description
+                    </p>
+                </div>`
+            }
             ;
         });
         //write each videos into html
-        videoContent.innerHTML = videos;
+        scratchContent.innerHTML = scratchVideos;
+        pythonContent.innerHTML = pythonVideos;
+        robloxContent.innerHTML = robloxVideos;
     }
     else {
-        videos += `
+        scratchVideos += `
         <div class="individual_project">
             <p>Videos not fund</p>
         </div>`
